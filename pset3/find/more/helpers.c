6fdaf2489c352b0b414c3b39d3b0788664d45f66
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2018-01-12 20:31:51 +0530

    projects: find, game of fifteen

---
+++ pset3/find/more/helpers.c
@@ -0,0 +1,74 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+
+#include <cs50.h>
+#include <math.h>
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // searching number using bool search
+    if (n > 1)
+    {
+        if (value == values[n / 2])
+        {
+            return true;
+        }
+        else
+        {
+            if (value > values[n / 2])
+            {
+                if (search(value, &values[n / 2], round(n / 2.0)))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (search(value, values, n / 2))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    else
+    {
+        if (value == values[0])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement a sorting algorithm
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (values[i] > values[j])
+            {
+                int temp = values[j];
+                values[j] = values[i];
+                values[i] = temp;
+            }
+        }
+    }
+    return;
+}
